6f51cbfd9538686fd422ca193e3ee574e3827360
H_Satt <hatosatoimo@outlook.jp>
2024-08-24 18:15:02 +0900

    Removed unused ifs

--- cogs/diyembed.py
+++ cogs/diyembed.py
@@ -10,8 +10,8 @@ class diyembed(commands.Cog):
     @commands.command(
         name="embed", # コマンドの名前。設定しない場合は関数名
     )
-    async def embed(self, ctx, title=None, description=None, title_url=None, author_name=None, author_url=None,
-                    author_icon=zunda, thumbnail=zunda, image=zunda, field1_name='', field1_value='', field2_name='', field2_value='',
+    async def embed(self, ctx, title='', description='', title_url='', author_name='', author_url='',
+                    author_icon='', thumbnail='', image='', field1_name='', field1_value='', field2_name='', field2_value='',
                     footer_text="Pasted by Satt", footer_icon=zunda):
         self.embed = discord.Embed( # Embedを定義する
                               title = title,# タイトル
@@ -27,11 +27,8 @@ class diyembed(commands.Cog):
             self.embed.set_thumbnail(url = thumbnail) # サムネイルとして小さい画像を設定できる
         if image != None:
             self.embed.set_image(url = image) # 大きな画像タイルを設定できる
-        if field1_name or field1_value != None:
             self.embed.add_field(name = field1_name, value = field1_value) # フィールドを追加。
-        if field2_name or field2_value != None:
             self.embed.add_field(name = field2_name, value = field2_value)
-        if footer_text or footer_icon != None:
             self.embed.set_footer(text = footer_text, # フッターには開発者の情報でも入れてみる
                                 icon_url = footer_icon)
         await ctx.send(embed=self.embed)
