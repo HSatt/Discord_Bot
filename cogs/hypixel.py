64e42b010ff31b9911be03bd322da0fd964d0ac0
H_Satt <hatosatoimo@outlook.jp>
2024-08-28 20:17:48 +0900

    Made *zombies into a bill

--- cogs/hypixel.py
+++ cogs/hypixel.py
@@ -99,14 +99,31 @@ class hypixel(commands.Cog): # xyzはcogの名前(ファイル名と同じにす
             await ctx.reply('fuck you idiot')
         embed = self.bot.get_command("embed")
         zom_stats = {"""\- D/W Ratio: """: "dwratio",
-                     "\n┗Wins: ": "wins_zombies", "\nᅠ┗Wins in DeadEnd: ": "wins_zombies_deadend", "\nᅠᅠ┣Wins in DE Normal: ": "wins_zombies_deadend_normal",
+                     "\n┗Wins: ": "wins_zombies",
+                     "\nᅠ┗Wins in DeadEnd: ": "wins_zombies_deadend", "\nᅠᅠ┣Wins in DE Normal: ": "wins_zombies_deadend_normal",
                      "\nᅠᅠ┣Wins in DE Hard: ": "wins_zombies_deadend_hard", "\nᅠᅠ┗Wins in DE RIP: ": "wins_zombies_deadend_rip",
                      "\nᅠ┗Wins in BadBlood: ": "wins_zombies_badblood", "\nᅠᅠ┣Wins in BB Normal: ": "wins_zombies_badblood_normal",
                      "\nᅠᅠ┣Wins in BB Hard: ": "wins_zombies_badblood_hard", "\nᅠᅠ┗Wins in BB RIP: ": "wins_zombies_badblood_rip",
                      "\nᅠ┗Wins in Alien Arcadium: ": "wins_zombies_alienarcadium",
                      "\nᅠ┗Wins in Prison: ": "wins_zombies_prison", "\nᅠᅠ┣Wins in Normal: ": "wins_zombies_prison_normal", 
                      "\nᅠᅠ┣Wins in Hard: ": "wins_zombies_prison_hard", "\nᅠᅠ┗Wins in RIP: ": "wins_zombies_prison_rip",
-                     "\n┗Deaths: ": "deaths_zombies"
+                     "\n┗Deaths: ": "deaths_zombies",
+                     "\nᅠ┗Deaths in DeadEnd: ": "deaths_zombies_deadend", "\nᅠᅠ┣Deaths in DE Normal: ": "deaths_zombies_deadend_normal",
+                     "\nᅠᅠ┣Deaths in DE Hard: ": "deaths_zombies_deadend_hard", "\nᅠᅠ┗Deaths in DE RIP: ": "deaths_zombies_deadend_rip",
+                     "\nᅠ┗Deaths in BadBlood: ": "deaths_zombies_badblood", "\nᅠᅠ┣Deaths in BB Normal: ": "deaths_zombies_badblood_normal",
+                     "\nᅠᅠ┣Deaths in BB Hard: ": "deaths_zombies_badblood_hard", "\nᅠᅠ┗Deaths in BB RIP: ": "deaths_zombies_badblood_rip",
+                     "\nᅠ┗Deaths in Alien Arcadium: ": "deaths_zombies_alienarcadium",
+                     "\nᅠ┗Deaths in Prison: ": "deaths_zombies_prison", "\nᅠᅠ┣Deaths in Normal: ": "deaths_zombies_prison_normal", 
+                     "\nᅠᅠ┣Deaths in Hard: ": "deaths_zombies_prison_hard", "\nᅠᅠ┗Deaths in RIP: ": "deaths_zombies_prison_rip",
+                     """\n\- K/D Ratio: """: "kdratio",
+                     "\n┗Kills: ": "zombie_kills_zombies",
+                     "\nᅠ┗Kills in DeadEnd: ": "zombie_kills_zombies_deadend", "\nᅠᅠ┣Kills in DE Normal: ": "zombie_kills_zombies_deadend_normal",
+                     "\nᅠᅠ┣Kills in DE Hard: ": "zombie_kills_zombies_deadend_hard", "\nᅠᅠ┗Kills in DE RIP: ": "zombie_kills_zombies_deadend_rip",
+                     "\nᅠ┗Kills in BadBlood: ": "zombie_kills_zombies_badblood", "\nᅠᅠ┣Kills in BB Normal: ": "zombie_kills_zombies_badblood_normal",
+                     "\nᅠᅠ┣Kills in BB Hard: ": "zombie_kills_zombies_badblood_hard", "\nᅠᅠ┗Kills in BB RIP: ": "zombie_kills_zombies_badblood_rip",
+                     "\nᅠ┗Kills in Alien Arcadium: ": "zombie_kills_zombies_alienarcadium",
+                     "\nᅠ┗Kills in Prison: ": "zombie_kills_zombies_prison", "\nᅠᅠ┣Kills in Normal: ": "zombie_kills_zombies_prison_normal", 
+                     "\nᅠᅠ┣Kills in Hard: ": "zombie_kills_zombies_prison_hard", "\nᅠᅠ┗Kills in RIP: ": "zombie_kills_zombies_prison_rip",
                      }
         desc = ''
         for key, value in zom_stats.items():
@@ -115,7 +132,7 @@ class hypixel(commands.Cog): # xyzはcogの名前(ファイル名と同じにす
                 if value == "dwratio":
                     desc += str(round(stats["deaths_zombies"] / stats["wins_zombies"], 3))
                 elif value == "kdratio":
-                    desc += str(round(stats["kills"] / stats["deaths"], 3))
+                    desc += str(round(stats["zombie_kills_zombies"] / stats["deaths_zombies"], 3))
                 else:
                     desc += str(stats[value])
             except KeyError:
