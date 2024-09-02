e8d71fda71d8c5d30bd49f0f21654dd7876d8736
H_Satt <hatosatoimo@outlook.jp>
2024-09-02 15:00:19 +0900

    Added *nowplaying, *getme

--- cogs/natori_button.py
+++ cogs/natori_button.py
@@ -6,6 +6,8 @@ import random
 from cogs.diyembed import diyembed
 from cogs.remusic import Music
 import math
+from mutagen.mp3 import MP3
+now_playing = ''
 path = "./data/sana"
 dirs = [f for f in os.listdir(path) if os.path.isdir(path + "/" + f)]
 voices = []
@@ -63,6 +65,8 @@ class natori_button(commands.Cog): # xyzはcogの名前(ファイル名と同じ
                 await Music.stop(self, ctx)
                 return
             source = natori_queue.pop(0)
+            global now_playing
+            now_playing = source
         source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(source))
         ctx.voice_client.play(source, 
                               after=lambda e: print(f'Player error: {e}') if e else self.bot.loop.create_task(self.nplayer(ctx, current=current)))
@@ -172,6 +176,37 @@ class natori_button(commands.Cog): # xyzはcogの名前(ファイル名と同じ
             await ctx.send(embed=await diyembed.getembed(self, color=0x1084fd, 
                                                                       description=reciept[3500 * (i + 1) + prev_check:],
                                                                       footer_text="Pasted by Satt", footer_icon=zunda))
+    @commands.command(aliases=['np'])
+    async def nowplaying(self, ctx):
+        try:
+            file = discord.File(now_playing)
+            await ctx.send(file=file, embed=await diyembed.getembed(self, title=f"""Now Playing...""", color=0x1084fd, 
+                                                    description=f"{now_playing}, {round(await self.length(query=str(now_playing)), 3)}s", 
+                                                    author_name='Soundboard bot for poors', author_url='https://satt.carrd.co/', 
+                                                    author_icon=zunda, thumbnail=zunda,
+                                                    ))
+        except FileNotFoundError:
+            await ctx.reply('Not Playing!')
+            return
+        
+    @commands.command()
+    async def getme(self, ctx, query):
+        try:
+            query = path + "/" + query
+            file = discord.File(query)
+            await ctx.send(file=file, embed=await diyembed.getembed(self, title=f"""Here you go.""", color=0x1084fd, 
+                                                    description=f"{query}, {round(await self.length(query=str(query)), 3)}s", 
+                                                    author_name='Soundboard bot for poors', author_url='https://satt.carrd.co/', 
+                                                    author_icon=zunda, thumbnail=zunda,
+                                                    ))
+        except FileNotFoundError:
+            await ctx.reply('File not found!')
+            return
+        
+    async def length(self, query):
+        """get length"""
+        audio = MP3(query)
+        return audio.info.length
     
     @commands.command()
     async def nshuffle(self, ctx):
@@ -184,4 +219,5 @@ class natori_button(commands.Cog): # xyzはcogの名前(ファイル名と同じ
         await ctx.reply("pong")
 
 async def setup(bot: commands.Bot):
+
     await bot.add_cog(natori_button(bot))
