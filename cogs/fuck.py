e14c1fb28a8b771e455c1d1ac70d4d1b527e9e46
H_Satt <hatosatoimo@outlook.jp>
2024-09-11 12:58:42 +0900

    Same reason

--- cogs/fuck.py
+++ cogs/fuck.py
@@ -31,11 +31,6 @@ items = [
     "アートのフレーム", "スポーツのメダル", "車のホイールリム", "歴史的な硬貨", 
     "装飾的な宝飾品", "高級時計のケース"
 ]
-trigger = {}
-with open("data/trigger.json", "r", encoding="utf-8") as f:
-# bank_info.jsonを開く(r)
-    trigger = json.load(f) # dataにファイル(f)をjsonとしてロードしたものを入れる
-print('Successfully loaded previous trigger record!')
 
 # MAKE IT COGGY
 class fuck(commands.Cog): # xyzはcogの名前(ファイル名と同じにすると良いぞ)(違っても良い)(好きにしな)
@@ -92,40 +87,6 @@ class fuck(commands.Cog): # xyzはcogの名前(ファイル名と同じにする
                 finally:
                     await asyncio.sleep(0.1)
         print(fucked)
-
-    @commands.command()
-    async def tag(self, ctx, mode, key, *, value=''):
-        if mode == 'create':
-            trigger[key] = value
-            print(f'Succesfully created: {key} for {trigger[key]}!')
-            try:
-                await ctx.reply(f'Succesfully created: {key} for {trigger[key]}!')
-            except discord.HTTPException:
-                await ctx.reply('bros tag is longer than my pp :skull:')
-        elif mode == 'remove':
-            try:
-                removed = trigger.pop(key)
-                print(f'Succesfully removed: {key} for {removed}!')
-                await ctx.reply(f'Succesfully removed: {key} for {removed}!')
-            except KeyError:
-                await ctx.reply('The key you entered does not exist in the list.')
-                return
-        else:
-            await ctx.reply('You need to use either create or remove to use this command!')
-        with open("data/trigger.json", "w+", encoding="utf-8") as f:
-            json.dump(trigger, f)
-
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if message.author == self.bot.user or message.author.bot:
-            return
-        for key, value in trigger.items():
-            if key in message.content:
-                try:
-                    await message.channel.send(value)
-                except discord.HTTPException:
-                    await message.channel.send('bros tag is longer than my pp :skull:')
-
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(fuck(bot))
